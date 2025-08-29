fixed dropout and implemented attention mask in tokenizer

tfs_mt/architecture.py
@@ -37,16 +37,14 @@ class MultiHeadAttention(nn.Module):
     Args:
         d_model (int): Model dimension.
         num_heads (int): Number of attention heads.
-        dropout_prob (float, optional): Dropout probability. Defaults to 0.1.
     """
 
-    def __init__(self, d_model: int, num_heads: int, dropout_prob: float = 0.1):
+    def __init__(self, d_model: int, num_heads: int):
         super().__init__()
 
         self.d_model = d_model
         self.num_heads = num_heads
         self.d_head = int(d_model / num_heads)  # Query, key and value embeddings dimension. d_k = d_v = d_head
-        self.dropout_prob = dropout_prob  # TODO Use it
 
         # Learnable projection matrices. Bias term is omitted since they are used as projections matrices.
         # Every head should have its projection matrix, but rather considering a set of QKV matrices for each head,
@@ -81,7 +79,6 @@ class MultiHeadAttention(nn.Module):
         Returns:
             torch.Tensor: Processed output tensor.
         """
-
         batch_size = x_query.shape[0]
 
         # W_Q(x)          [B, S, D]
@@ -122,7 +119,6 @@ class MultiHeadAttention(nn.Module):
         Returns:
             torch.Tensor: Attention matrix.
         """
-
         QKt = torch.matmul(query, key.transpose(-2, -1)) / self.scaling_factor
 
         print(QKt)  # debug
@@ -184,17 +180,22 @@ class EncoderBlock(nn.Module):
     def __init__(self, d_model: int, num_heads: int, d_ff: int, dropout_prob: float = 0.1):
         super().__init__()
 
-        self.self_attention = MultiHeadAttention(d_model, num_heads, dropout_prob)
+        self.self_attention = MultiHeadAttention(d_model, num_heads)
         self.feedforward = FeedForward(d_model, d_ff, dropout_prob)
         self.layer_norm1 = LayerNorm(d_model)
         self.layer_norm2 = LayerNorm(d_model)
+        self.dropout = nn.Dropout(dropout_prob)
 
     def forward(self, x: torch.Tensor, attention_mask: torch.BoolTensor) -> torch.Tensor:
         t1 = self.layer_norm1(x)
         t2 = self.self_attention(x_query=t1, x_key=t1, x_value=t1, attention_mask=attention_mask)
+        # We apply dropout [33] to the output of each sub-layer, before it is added to the sub-layer input and normalized (Attentio is all you need page 8)
+        t2 = self.dropout(t2)
         t3 = t2 + x
+
         t4 = self.layer_norm2(t3)
         t5 = self.feedforward(t4)
+        t5 = self.dropout(t5)
         h = t5 + t3
 
         return h
@@ -215,25 +216,31 @@ class DecoderBlock(nn.Module):
     def __init__(self, d_model: int, num_heads: int, d_ff: int, dropout_prob: float = 0.1):
         super().__init__()
 
-        self.self_attention = MultiHeadAttention(d_model, num_heads, dropout_prob)
-        self.cross_attention = MultiHeadAttention(d_model, num_heads, dropout_prob)
+        self.self_attention = MultiHeadAttention(d_model, num_heads)
+        self.cross_attention = MultiHeadAttention(d_model, num_heads)
         self.feedforward = FeedForward(d_model, d_ff, dropout_prob)
         self.layer_norm1 = LayerNorm(d_model)
         self.layer_norm2 = LayerNorm(d_model)
         self.layer_norm3 = LayerNorm(d_model)
+        self.dropout = nn.Dropout(dropout_prob)
 
     def forward(self, x: torch.Tensor, encoder_output: torch.Tensor, attention_mask: torch.BoolTensor) -> torch.Tensor:
         t1 = self.layer_norm1(x)
         t2 = self.self_attention(x_query=t1, x_key=t1, x_value=t1, attention_mask=attention_mask)
+        # We apply dropout [33] to the output of each sub-layer, before it is added to the sub-layer input and normalized (Attentio is all you need page 8)
+        t2 = self.dropout(t2)
         t3 = t2 + x
+
         t4 = self.layer_norm2(t3)
-        # TODO check mask
         t5 = self.cross_attention(
             x_query=t4, x_key=encoder_output, x_value=encoder_output, attention_mask=attention_mask
         )
+        t5 = self.dropout(t5)
         t6 = t5 + t3
+
         t7 = self.layer_norm3(t6)
         t8 = self.feedforward(t7)
+        t8 = self.dropout(t8)
         h = t8 + t6
 
         return h
@@ -296,8 +303,8 @@ class Transformer(nn.Module):
         else:
             self.tgt_embeddings = Embedding(tgt_vocab_size, d_model)
 
-        self.src_pos_embeddings = SinusoidalPositionalEncoding(d_model)
-        self.tgt_pos_embeddings = SinusoidalPositionalEncoding(d_model)
+        self.src_pos_embeddings = SinusoidalPositionalEncoding(d_model, dropout_prob)
+        self.tgt_pos_embeddings = SinusoidalPositionalEncoding(d_model, dropout_prob)
 
         self.encoder = nn.ModuleList([
             EncoderBlock(d_model, num_heads, d_ff, dropout_prob) for _ in range(num_encoder_blocks)
@@ -314,9 +321,8 @@ class Transformer(nn.Module):
         This weight initialization strategy was first introduced [here](https://proceedings.mlr.press/v9/glorot10a.html). It's used to stabilize gradients during training.
 
         Args:
-            skip_embeddings (str | None, optional): _description_. Defaults to None.
+            skip_embeddings (str | None, optional): Which type of embedding should skip Xavier initialization. Useful when initializing embeddings with GloVe. Defaults to None.
         """
-
         for name, p in self.named_parameters():
             if skip_embeddings is not None and f"{skip_embeddings}_embeddings" in name:
                 continue

tfs_mt/data_utils.py
@@ -159,7 +159,7 @@ class WordTokenizer:
     def build_vocab_parallel(
         self,
         tokens: list[str],
-        min_freq: int = 1,
+        min_freq: int = 2,
         extend_with_glove: bool = False,
         glove_version: str = "glove.2024.wikigiga.50d",
         **kwargs,
@@ -168,7 +168,7 @@ class WordTokenizer:
 
         Args:
             tokens (list[str]): Tokens from dataset to build vocabulary on.
-            min_freq (int, optional): Minimum number of times a token has to appear in the dataset to be included in the vocabulary. Defaults to 1.
+            min_freq (int, optional): Minimum number of times a token has to appear in the dataset to be included in the vocabulary. Defaults to 2.
             extend_with_glove (bool, optional): Enable vocabulary extension with GloVe tokens. Defaults to False.
             glove_version (str, optional): GloVe version to use if `extend_with_glove` is `True`. Defaults to "glove.2024.wikigiga.50d".
 
@@ -278,7 +278,7 @@ class WordTokenizer:
     def build_vocab(
         self,
         tokens: list[str],
-        min_freq: int = 1,
+        min_freq: int = 2,
         extend_with_glove: bool = False,
         glove_version: str = "glove.2024.wikigiga.50d",
         **kwargs,
@@ -287,7 +287,7 @@ class WordTokenizer:
 
         Args:
             tokens (list[str]): Tokens from dataset to build vocabulary on.
-            min_freq (int, optional): Minimum number of times a token has to appear in the dataset to be included in the vocabulary. Defaults to 1.
+            min_freq (int, optional): Minimum number of times a token has to appear in the dataset to be included in the vocabulary. Defaults to 2.
             extend_with_glove (bool, optional): Enable vocabulary extension with GloVe tokens. Defaults to False.
             glove_version (str, optional): GloVe version to use if `extend_with_glove` is `True`. Defaults to "glove.2024.wikigiga.50d".
 
@@ -374,7 +374,7 @@ class WordTokenizer:
 
         print(f"Built vocabulary with {len(vocab)} tokens.")
 
-    def encode(self, text: str, pad_to_len: int | None = None) -> list[int]:
+    def encode(self, input_sequence: str | list[str], pad_to_len: int | None = None) -> tuple[list[int], list[bool]]:
         """Encode text to token IDs.
 
         Args:
@@ -389,9 +389,10 @@ class WordTokenizer:
         if self.vocab_size == 0:
             raise VocabNotBuiltError()
 
-        tokens = self.tokenize(text)
+        # Useful when building a TranslationDataset in order to execute tokenize method only once
+        tokens = self.tokenize(input_sequence) if isinstance(input_sequence, str) else input_sequence
 
-        # Add SOS and EOS tokens for target sequences
+        # Add SOS and EOS tokens to given sequence
         tokens.insert(0, self.special_tokens["sos_token"])
         tokens.append(self.special_tokens["eos_token"])
 
@@ -402,19 +403,15 @@ class WordTokenizer:
             for token in tokens
         ]
 
-        # token_ids = []
-        # for token in tokens:
-        #     if token in self.token_to_idx:
-        #         token_ids.append(self.token_to_idx[token])
-        #     else:
-        #         token_ids.append(self.token_to_idx[self.special_tokens["unk_token"]])
-
         if pad_to_len is not None:  # Pad sequence to pad_to_len
             token_ids.extend([
                 self.token_to_idx[self.special_tokens["pad_token"]] for _ in range(pad_to_len - len(tokens))
             ])
 
-        return token_ids
+        # Disabling attention to pad tokens
+        mask = [token != self.token_to_idx[self.special_tokens["pad_token"]] for token in token_ids]
+
+        return token_ids, mask
 
     def decode(self, token_ids: list[int]) -> list[str]:
         """Decode token IDs.
@@ -444,7 +441,7 @@ class TranslationDataset(Dataset):
         src_lang (str): Identifier for the source language, e.g., `"en"` for English.
         tgt_lang (str): Identifier for the target language, e.g., `"it"` for Italian.
         max_length (int | None, optional): Maximum sequence length for tokenization. If None, sequences are not truncated. Defaults to None.
-        vocab_min_freq (int, optional): Minimum frequency threshold for including a token in the vocabulary. Defaults to 1.
+        vocab_min_freq (int, optional): Minimum frequency threshold for including a token in the vocabulary. Defaults to 2.
         extend_vocab_with_glove (bool, optional): Whether to extend the vocabulary with pretrained GloVe embeddings. Defaults to False.
     """
 
@@ -456,7 +453,7 @@ class TranslationDataset(Dataset):
         src_lang: str,
         tgt_lang: str,
         max_length: int | None = None,
-        vocab_min_freq: int = 1,
+        vocab_min_freq: int = 2,
         extend_vocab_with_glove: bool = False,
         **kwargs,
     ):
@@ -483,7 +480,7 @@ class TranslationDataset(Dataset):
         else:
             self._build_vocabs(vocab_min_freq=vocab_min_freq, extend_with_glove=extend_vocab_with_glove)
 
-    def _build_vocabs(self, vocab_min_freq: int = 1, extend_with_glove: bool = False, **kwargs) -> None:
+    def _build_vocabs(self, vocab_min_freq: int = 2, extend_with_glove: bool = False, **kwargs) -> None:
         """Build vocabularies for tokenizers."""
 
         print("Building vocabs, it may take a few minutes...")
@@ -519,13 +516,20 @@ class TranslationDataset(Dataset):
         src_text = example["translation"][self.src_lang]
         tgt_text = example["translation"][self.tgt_lang]
 
+        # src and tgt sequence lengths must be the same to properly compute cross attention
+        # The smaller sequence will be padded to the length of the longer sequence
+        # Attention mask ensure no attention is computed with pad tokens
+        max_seq_len = max(len(src_text), len(tgt_text))
+
         # Tokenize texts
-        src_tokens = self.src_tokenizer.encode(src_text)
-        tgt_tokens = self.tgt_tokenizer.encode(tgt_text)
+        src_tokens, src_mask = self.src_tokenizer.encode(src_text, pad_to_len=max_seq_len)
+        tgt_tokens, src_mask = self.tgt_tokenizer.encode(tgt_text, pad_to_len=max_seq_len)
 
         return {
             "src": torch.tensor(src_tokens, dtype=torch.long),
             "tgt": torch.tensor(tgt_tokens, dtype=torch.long),
+            "src_mask": torch.tensor(src_mask, dtype=torch.bool),
+            "tgt_mask": torch.tensor(src_mask, dtype=torch.bool),
             "src_text": src_text,
             "tgt_text": tgt_text,
         }

tfs_mt/embeddings.py
@@ -95,9 +95,8 @@ class Embedding(nn.Module):
 
         else:
             embeddings_dim = d_model
-            embeddings_lut = nn.Embedding(
-                vocab_size, d_model
-            )  # Randomly initialized lookup table. If so the initialization will be handled in architecture.Transformer class
+            # Randomly initialized lookup table. If so the initialization will be handled in architecture.Transformer class
+            embeddings_lut = nn.Embedding(vocab_size, d_model)
 
         self.d_model = embeddings_dim
         self.embeddings_lut = embeddings_lut
@@ -115,7 +114,8 @@ class Embedding(nn.Module):
             with open(embeddings_path, encoding="utf-8") as f:
                 for line in f:
                     parts = line.strip().split()
-                    idx = tokenizer.encode(parts[0])[1]  # The first token coming out of tokenizer.encode is SOS_TOKEN
+                    idx, _ = tokenizer.encode(parts[0])
+                    idx = idx[1]  # The first token coming out of tokenizer.encode is SOS_TOKEN
                     if len(parts[1:]) != embeddings_dim:  # Skip unhandled tokens with spaces, eg. "1 3/4"
                         continue
                     try:
@@ -156,10 +156,10 @@ class SinusoidalPositionalEncoding(nn.Module):
 
     Args:
         d_model (int): Model dimension.
-        max_sequence_length (int, optional): Max sequence length. Defaults to 512.
+        max_sequence_length (int, optional): Max sequence length. Defaults to 128.
     """
 
-    def __init__(self, d_model: int, max_sequence_length: int = 512):
+    def __init__(self, d_model: int, dropout_prob: float = 0.1, max_sequence_length: int = 128):
         super().__init__()
 
         # Vector of all possible positions in the sequence [max_sequence_length, 1]
@@ -175,10 +175,15 @@ class SinusoidalPositionalEncoding(nn.Module):
         # but they won't appear in model.parameters so that optimizer will not change them
         self.register_buffer("pe_lut", pe_lut)
 
+        self.dropout = nn.Dropout(dropout_prob)
+
     def forward(self, token_embeddings: nn.Embedding) -> nn.Embedding:
         if token_embeddings.ndim != 3 or token_embeddings.size(-1) != self.pe_lut.size(1):
             raise IncompatibleEmbeddingsDimError(token_embeddings.shape)
 
         positional_encodings = self.pe_lut[: token_embeddings.size(1)]
 
-        return token_embeddings + positional_encodings
+        # we apply dropout to the sums of the embeddings and the positional encodings in both the encoder and decoder stacks (Attention is all you need page 8)
+        final_embedding = self.dropout(token_embeddings + positional_encodings)
+
+        return final_embedding