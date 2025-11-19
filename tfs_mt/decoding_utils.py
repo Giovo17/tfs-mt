8631bdb691c39294e58a1d387f7f2edd62758f55
Giovo17 <19341666+Giovo17@users.noreply.github.com>
2025-11-20 00:48:09 +0100

    improved and simplified interface in greedy decoding

--- tfs_mt/decoding_utils.py
+++ tfs_mt/decoding_utils.py
@@ -1,6 +1,16 @@
+# Transformer decoding utils
+#
+# Author: Giovanni Spadaro - https://giovannispadaro.it
+# Project: https://github.com/Giovo17/tfs-mt
+# Documentation: https://giovo17.github.io/tfs-mt
+#
+# Copyright (c) Giovanni Spadaro.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the LICENSE file in the root directory of this source tree.
+
+import numpy as np
 import torch
-from omegaconf.dictconfig import DictConfig
-from omegaconf.listconfig import ListConfig
 
 from .architecture import Transformer
 from .data_utils import WordTokenizer
@@ -8,13 +18,13 @@ from .data_utils import WordTokenizer
 
 @torch.inference_mode()
 def greedy_decoding(
-    config: DictConfig | ListConfig,
     model: Transformer,
-    encoder_representation: torch.Tensor,
-    src_mask: torch.Tensor,
     tgt_tokenizer: WordTokenizer,
+    src_tokens: np.ndarray | torch.Tensor,
+    src_mask: np.ndarray | torch.Tensor,
     max_target_tokens: int = 128,
-) -> list[str]:
+    output_mode: str = "str",
+) -> list[str] | list[list[str]]:
     """
     Supports batch (decode multiple source sentences) greedy decoding.
 
@@ -27,56 +37,37 @@ def greedy_decoding(
     activations will remain the same as it only looks at/attends to itself and to `<s>` and so forth.
 
     Args:
-        config (DictConfig | ListConfig): _description_
-        model (Transformer): _description_
-        encoder_representation (torch.Tensor): _description_
-        src_mask (torch.Tensor): _description_
-        tgt_tokenizer (WordTokenizer): _description_
-        max_target_tokens (int, optional): _description_. Defaults to 128.
+        model (Transformer): Encoder-decoder translation model.
+        tgt_tokenizer (WordTokenizer): Target text tokenizer.
+        src_tokens (torch.Tensor): Source tokens.
+        src_mask (torch.Tensor): Source tokens mask.
+        max_target_tokens (int, optional): Max target tokens to output. Defaults to 128.
+        output_mode (str, optional): Output mode, if `str` it will return the sequence as a string, if `tokens` it will return as list of tokens. Defaults to "str".
 
     Returns:
-        list[str]: _description_
-
-    Examples:
-        ```
-        device = "cuda:0" if torch.cuda.is_available() else "cpu"
-        config = OmegaConf.load("path/to/config")
-
-        _, _, _, _, src_tokenizer, tgt_tokenizer = build_data_utils(config, return_all=True)
-
-        src_sequences = [
-            "The bottle floated out in the river",
-            "The green witch arrived"
-        ]
-        src_sequences_enc, src_masks = src_tokenizer.encode(src_sequences)
-        src_sequences_enc = torch.tensor(src_sequences_enc, device=device)
-        src_masks = torch.tensor(src_masks, device=device)
-
-        model = build_model(config, src_tokenizer, tgt_tokenizer)
-
-        # Load model from pretrained
-
-        model = model.to(device)
-        model = model.eval()
-
-        with torch.inference_mode():
-            encoder_representation = model.encode(src_sequences, src_masks)
-
-        encoded_sequence = greedy_decoding(
-            config=config,
-            model=model,
-            encoder_representation=encoder_representation,
-            src_mask=src_masks,
-            tgt_tokenizer=tgt_tokenizer,
-            max_target_tokens=config.tokenizer.max_seq_len
-        )
-        ```
+        list[str] | list[list[str]]: Decoded sequences as strings or as list of tokens.
     """
 
     device = next(model.parameters()).device
 
+    sos_token = tgt_tokenizer.decode([tgt_tokenizer.sos_token_idx])[0]
+    eos_token = tgt_tokenizer.decode([tgt_tokenizer.eos_token_idx])[0]
+
+    # Get encoder representation
+    if isinstance(src_tokens, np.ndarray) and isinstance(src_tokens, np.ndarray):
+        src_tokens = torch.tensor(src_tokens)
+        src_mask = torch.tensor(src_mask)
+    if src_tokens.ndim == 1 and src_mask.ndim == 1:
+        src_tokens.unsqueeze_(0)
+        src_mask.unsqueeze_(0)
+
+    src_tokens = src_tokens.to(device)
+    src_mask = src_mask.to(device)
+
+    encoder_representation = model.encode(src_tokens, src_mask)
+
     # Generate a batch of sequences starting with SOS token, batch size is inferred by the encoder representation tensor
-    tgt_sequence_batch_text = [[config.tokenizer.sos_token] for _ in range(encoder_representation.shape[0])]
+    tgt_sequence_batch_text = [[sos_token] for _ in range(encoder_representation.shape[0])]
     tgt_sequence_batch = torch.tensor(
         [[tgt_tokenizer.sos_token_idx] for _ in range(encoder_representation.shape[0])], device=device
     )
@@ -113,10 +104,8 @@ def greedy_decoding(
 
         for idx, predicted_word in enumerate(predicted_words):
             tgt_sequence_batch_text[idx].append(predicted_word)
-
-            if (
-                predicted_word == config.tokenizer.eos_token
-            ):  # Once EOS token is generated for a sentence in the batch it gets flagged in is_decoded list
+            # Once EOS token is generated for a sentence in the batch it gets flagged in is_decoded list
+            if predicted_word == eos_token:
                 is_decoded[idx] = True
 
         if all(is_decoded) or num_of_tgt_tokens == max_target_tokens:
@@ -131,24 +120,35 @@ def greedy_decoding(
     post_processed_sequences = []
     for tgt_sequence in tgt_sequence_batch_text:
         try:
-            target_index = tgt_sequence.index(config.tokenizer.eos_token) + 1
+            target_index = tgt_sequence.index(eos_token) + 1
         except ValueError:
             target_index = None
 
         tgt_sequence = tgt_sequence[:target_index]
         post_processed_sequences.append(tgt_sequence)
 
+    if output_mode == "str":
+        post_processed_sequences_str = []
+        for i in range(len(post_processed_sequences)):
+            seq = post_processed_sequences[i]
+            seq.remove(sos_token)
+            if eos_token in seq:
+                seq.remove(eos_token)
+            sequence_as_str = " ".join(seq)
+            post_processed_sequences_str.append(sequence_as_str)
+        post_processed_sequences = post_processed_sequences_str
+
     return post_processed_sequences
 
 
 @torch.inference_mode()
 def beam_decoding(
-    config: DictConfig | ListConfig,
     model: Transformer,
-    encoder_representation: torch.Tensor,
-    src_mask: torch.Tensor,
     tgt_tokenizer: WordTokenizer,
+    src_tokens: np.ndarray | torch.Tensor,
+    src_mask: np.ndarray | torch.Tensor,
     max_target_tokens: int = 128,
+    output_mode: str = "str",
 ) -> list[str]:
     """
     TBA
